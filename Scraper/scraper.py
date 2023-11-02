050568cfb9235cd781aeb61e9817b87337c33499
Avital999 <avital1199@gmail.com>
2023-11-02 10:41:31 +0200

    scraper-temporary-ignore

--- Scraper/scraper.py
+++ Scraper/scraper.py
@@ -4,12 +4,18 @@ import requests
 import time
 
 
+
 def html_info():
     # Using selenium
     options = webdriver.ChromeOptions()
     options.add_argument("webdriver\chromedriver.exe")
     driver = webdriver.Chrome(options=options)
-    driver.get("https://parade.com/968666/parade/chuck-norris-jokes/")
+    url = "https://parade.com/968666/parade/chuck-norris-jokes/"
+    driver.get(url)
     print("STARTTTTTTTTT")
     time.sleep(10)
+    page_source = driver.page_source
+    print(page_source)
+    soup = BeautifulSoup(page_source, "lxml")
+    print("second")
     driver.quit()
