e8015a3c0e68cba9af025e6ddc4c669cd188309a
Avital999 <avital1199@gmail.com>
2023-11-03 11:49:54 +0200

    Update scraper.py

--- Scraper/scraper.py
+++ Scraper/scraper.py
@@ -1,32 +1,16 @@
 from bs4 import BeautifulSoup
-from selenium import webdriver
-import requests
-import cloudscraper
 import time
-from itertools import cycle
-
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.common.by import By
-from zenrows import ZenRowsClient
-
-URL = "https://parade.com/968666/parade/chuck-norris-jokes/"
-API_KEY = open("key.txt").read().strip() if open("key.txt").read().strip() else None
 
 def html_info():
-    url = URL
-    apikey = API_KEY
-    params = {
-        'url': url,
-        'apikey': apikey,
-        'js_render': 'true',
-        'antibot': 'true',
-        'wait': '1103',
-        'json_response': 'true',
-        'js_instructions': """[{"click":".selector"},{"wait":500},{"fill":[".input","value"]},{"wait_for":".slow_selector"}]""",
-    }
-    response = requests.get('https://api.zenrows.com/v1/', params=params)
-    print(response.text)
-    soup = BeautifulSoup(response.text ,'lxml').prettify()
-    print("----------------------")
+    # Using selenium
+    options = webdriver.ChromeOptions()
+    options.add_argument("webdriver\chromedriver.exe")
+    driver = webdriver.Chrome(options=options)
+    url = "https://www.liveabout.com/top-chuck-norris-jokes-2307758"
+    driver.get(url)
+    time.sleep(10.23)
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, "lxml")
     print(soup)
+    driver.quit()
