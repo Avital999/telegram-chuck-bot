fc808914f151e04266bf9598eba9d725820af5f7
Avital999 <avital1199@gmail.com>
2023-11-04 16:38:27 +0200

    Update translator.py

--- translator.py
+++ translator.py
@@ -2,6 +2,12 @@ import boto3
 import langcodes
 
 
+def is_rtl_language(language):
+    rtl_languages = ['arabic', 'hebrew', 'persian', 'urdu', 'yiddish', 'sindhi',
+                     'dhivehi', 'pashto', 'kurdish', 'sorani', 'kurmanji', 'uighur', 'jawi', 'thaana']
+    return language.lower() in rtl_languages
+
+
 def language_name_to_iso_code(language_name):
     try:
         code = langcodes.find(language_name).language
@@ -16,8 +22,10 @@ def translate_text(text: str, target_language: str) -> str:
     result = translate.translate_text(Text=text,
                                       SourceLanguageCode="en",
                                       TargetLanguageCode=language_name_to_iso_code(target_language))
-    print('TranslatedText: ' + result.get('TranslatedText'))
-    return result.get('TranslatedText')
+    translated_text: str = result.get('TranslatedText')
+    if is_rtl_language(target_language):
+        translated_text = translated_text[::-1]
+    print(translated_text)
+    return translated_text
 
 
-translate_text(text='My name is Dana',target_language='Hebrew')
