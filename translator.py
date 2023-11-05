842354046a8c7336eb420fec93ece92d486681ea
Avital999 <avital1199@gmail.com>
2023-11-05 13:05:22 +0200

    Update translator.py

--- translator.py
+++ translator.py
@@ -1,5 +1,8 @@
+import os
+
 import boto3
 import langcodes
+from secret import aws_secret_access_key, aws_access_key_id
 
 
 def language_name_to_iso_code(language_name):
@@ -13,7 +16,10 @@ def language_name_to_iso_code(language_name):
 def translate_text(text: str, target_language: str) -> str:
     """ Given a text and a target language, using AWS translate services,
     translate the given text to the language."""
-    translate = boto3.client(service_name='translate', region_name='us-east-1', use_ssl=True)
+
+    translate = boto3.client(service_name='translate', region_name='us-east-1', use_ssl=True,
+                             aws_access_key_id=aws_access_key_id(),
+                             aws_secret_access_key=aws_secret_access_key())
 
     result = translate.translate_text(Text=text,
                                       SourceLanguageCode="en",
