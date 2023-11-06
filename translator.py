003928ceb9a68e0f3ff1387b967ea38d6e182922
Avital999 <avital1199@gmail.com>
2023-11-06 12:24:54 +0200

    Encryption

--- translator.py
+++ translator.py
@@ -1,8 +1,6 @@
-import os
-
 import boto3
 import langcodes
-from secret import aws_secret_access_key, aws_access_key_id
+from cryptography.fernet import Fernet
 
 
 def language_name_to_iso_code(language_name):
@@ -13,13 +11,43 @@ def language_name_to_iso_code(language_name):
         return None
 
 
+def access_key():
+    # Read the encryption key from the 'encryptionkey.txt' file
+    with open("encryptionkey.txt", "rb") as key_file:
+        encryption_key = key_file.read()
+
+    # Read the encrypted keys from the 'encrypted_keys.txt' file
+    with open("encrypted_keys.txt", "rb") as file:
+        lines = file.readlines()
+
+    # Decrypt the AWS access key and decode it to a UTF-8 string
+    cipher_suite = Fernet(encryption_key)
+    access_key = cipher_suite.decrypt(lines[0].strip()).decode('utf-8')
+    return access_key
+
+
+def secret_key():
+    # Read the encryption key from the 'encryptionkey.txt' file
+    with open("encryptionkey.txt", "rb") as key_file:
+        encryption_key = key_file.read()
+
+    # Read the encrypted keys from the 'encrypted_keys.txt' file
+    with open("encrypted_keys.txt", "rb") as file:
+        lines = file.readlines()
+
+    # Decrypt the AWS secret key and decode it to a UTF-8 string
+    cipher_suite = Fernet(encryption_key)
+    secret_key = cipher_suite.decrypt(lines[1].strip()).decode('utf-8')
+    return secret_key
+
+
 def translate_text(text: str, target_language: str) -> str:
     """ Given a text and a target language, using AWS translate services,
     translate the given text to the language."""
 
     translate = boto3.client(service_name='translate', region_name='us-east-1', use_ssl=True,
-                             aws_access_key_id=aws_access_key_id(),
-                             aws_secret_access_key=aws_secret_access_key())
+                             aws_access_key_id=access_key(),
+                             aws_secret_access_key=secret_key())
 
     result = translate.translate_text(Text=text,
                                       SourceLanguageCode="en",
