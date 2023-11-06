e1ea0e5e486122fef0cd32bfc944176975f6d103
Avital999 <avital1199@gmail.com>
2023-11-06 22:21:52 +0200

    Update manage_lanagues.py
    refactoring

--- manage_lanagues.py
+++ manage_lanagues.py
@@ -1,16 +1,21 @@
 import csv
-import os
 from typing import Final
 
-
+FIELD_NAMES = ['user_id', 'language']
 LANGUAGES_CSV: Final = 'languages.csv'
 
 
-def create_csv():
-    with open(LANGUAGES_CSV, 'w', newline='') as csv_file:
-        fieldnames = ['user_id', 'language']
-        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        csv_writer.writeheader()
+def update_user_preferred_language(user_id: int, language: str):
+    data = read_data_from_csv()
+    data = update_languages_list(data=data, user_id=user_id, language=language)
+    write_data_to_csv(data)
+
+
+def read_data_from_csv():
+    with open(LANGUAGES_CSV, 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        data = list(csv_reader)
+    return data
 
 
 def update_languages_list(data, user_id: int, language: str):
@@ -23,25 +28,29 @@ def update_languages_list(data, user_id: int, language: str):
     return data
 
 
-def update_user_preferred_language(user_id: int, language: str):
-    data = read_data_from_csv()
-    data = update_languages_list(data=data, user_id=user_id, language=language)
-    write_data_to_csv(data)
-
-
 def write_data_to_csv(data):
     with open(LANGUAGES_CSV, 'w', newline='') as csv_file:
-        fieldnames = ['user_id', 'language']
+        fieldnames = FIELD_NAMES
         csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         csv_writer.writeheader()
         csv_writer.writerows(data)
 
 
-def read_data_from_csv():
-    with open(LANGUAGES_CSV, 'r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        data = list(csv_reader)
-    return data
+def create_csv():
+    with open(LANGUAGES_CSV, 'w', newline='') as csv_file:
+        fieldnames = FIELD_NAMES
+        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        csv_writer.writeheader()
+
+
+
+
+
+
+
+
+
+
 
 
 
